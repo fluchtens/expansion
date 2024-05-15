b0fc1684bf3a50962815493f7c3b5619c1ccab64
Cleazyy <cleazy2002@gmail.com>
2024-05-15 12:21:58 +0200

    create custom 404 not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,11 @@
+import styles from "./not-found.module.scss";
+
+export default function NotFound() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.container}>
+        <h1>PAGE NON TROUVÉE</h1>
+      </div>
+    </main>
+  );
+}
