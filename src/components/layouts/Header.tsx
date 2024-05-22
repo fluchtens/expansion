08e8d5046bd6e8364fced9a2a3aa3800b9ca43c1
Cleazyy <cleazy2002@gmail.com>
2024-05-22 13:03:05 +0200

    create navigation responsive menu to header

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -1,23 +1,50 @@
-"use server";
+"use client";
 
 import Link from "next/link";
+import { useEffect, useRef, useState } from "react";
+import { AiOutlineMenu } from "react-icons/ai";
 import { NavLink } from "../ui/NavLink";
 import styles from "./Header.module.scss";
 
-export const Header = async () => {
+export const Header = () => {
+  const [navMenu, setNavMenu] = useState<boolean>(false);
+  const navMenuRef = useRef<HTMLDivElement>(null);
+
+  const toggleNavMenu = () => {
+    setNavMenu(!navMenu);
+  };
+
+  const closeNavMenu = () => {
+    setNavMenu(false);
+  };
+
+  useEffect(() => {
+    const handler = (e: MouseEvent) => {
+      if (navMenuRef.current) {
+        if (!navMenuRef.current.contains(e.target as Node)) {
+          closeNavMenu();
+        }
+      }
+    };
+    document.addEventListener("mousedown", handler);
+  }, []);
+
   return (
     <header className={styles.header}>
       <div className={styles.container}>
-        <div className={styles.primary}>
-          <Link href="/">
+        <div className={styles.primary} ref={navMenuRef}>
+          <Link href="/" className={styles.mainLink}>
             <img src="/logo.svg" alt="logo.svg" />
           </Link>
-          <ul className={styles.ul}>
-            <NavLink href="/" name="Bienvenue" />
-            <NavLink href="/metiers" name="Nos métiers" />
-            <NavLink href="/methodologie" name="Notre méthodologie" />
-            <NavLink href="/agence" name="L'agence" />
-            <NavLink href="/blog" name="Blog" />
+          <button className={styles.navBtn} onClick={toggleNavMenu}>
+            <AiOutlineMenu className={styles.navBtnIcon} />
+          </button>
+          <ul className={`${navMenu ? styles.navListMenu : styles.navList}`}>
+            <NavLink href="/" name="Bienvenue" callback={closeNavMenu} />
+            <NavLink href="/metiers" name="Nos métiers" callback={closeNavMenu} />
+            <NavLink href="/methodologie" name="Notre méthodologie" callback={closeNavMenu} />
+            <NavLink href="/agence" name="L'agence" callback={closeNavMenu} />
+            <NavLink href="/blog" name="Blog" callback={closeNavMenu} />
           </ul>
         </div>
         <div className={styles.secondary}>
--- src/components/ui/NavLink.tsx
+++ src/components/ui/NavLink.tsx
@@ -7,15 +7,20 @@ import styles from "./NavLink.module.scss";
 interface NavLinkProps {
   href: string;
   name: string;
+  callback: () => void;
 }
 
-export const NavLink = ({ href, name }: NavLinkProps) => {
+export const NavLink = ({ href, name, callback }: NavLinkProps) => {
   const path = usePathname();
   const isActive = path === href;
 
   return (
     <li>
-      <Link href={href} className={`${styles.link} ${isActive ? styles.active : styles.inactive}`}>
+      <Link
+        href={href}
+        onClick={callback}
+        className={`${styles.link} ${isActive ? styles.active : styles.inactive}`}
+      >
         {name}
       </Link>
     </li>
